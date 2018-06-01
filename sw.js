7a6db5cac5a481525f51b687784ebf327a585615
jeroenvanoers <jeroen.vanoers@ictmbo.nl>
2018-06-01 09:03:20 +0200

    push iets a neef

--- sw.js
+++ sw.js
@@ -1,12 +1,14 @@
-var CACHE_NAME = 'my-wandelapp-v1';
+var CACHE_NAME = 'my-wandelapp-v2';
 var urlsToCache = [
-    '/'
+    '/',
+    '/js/app_es5.js',
+    'https://nodejs-mongo-persistent-wandelappbackend-v4.a3c1.starter-us-west-1.openshiftapps.com'
 ];
 
 self.addEventListener('install', function(event) {
     // Perform install steps
     event.waitUntil(
-        caches.open(CACHE_NAME)
+        caches.open(CACHE_NAMEQ)
             .then(function(cache) {
                 console.log('Opened cache');
                 return cache.addAll(urlsToCache);
